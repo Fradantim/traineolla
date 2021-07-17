903812d246150be75295982149ae38e51c607938
Fradantim <fradantim@gmail.com>
2021-07-17 20:02:05 -0300

    Empieza el reactivismo

--- app/udemy-connector/src/main/java/com/frager/oreport/udemyconnector/client/UdemyClient.java
+++ app/udemy-connector/src/main/java/com/frager/oreport/udemyconnector/client/UdemyClient.java
@@ -10,15 +10,27 @@ import com.udemy.model.UserActivity;
 import com.udemy.model.UserCourseActivity;
 import com.udemy.model.UserProgress;
 
+import reactor.core.publisher.Mono;
+
 public interface UdemyClient {
 
-	public SingleCourse getCourseById(Integer id, @Nullable MultiValueMap<String, String> queryParams);
+	public Mono<SingleCourse> getCourseById(Integer id);
+	
+	public Mono<SingleCourse> getCourseById(Integer id, @Nullable MultiValueMap<String, String> queryParams);
+	
+	public PageResponse<ListedCourse> getCourses();
 	
 	public PageResponse<ListedCourse> getCourses(@Nullable MultiValueMap<String, String> queryParams);
 	
+	public PageResponse<UserActivity> getUserActivity();
+	
 	public PageResponse<UserActivity> getUserActivity(@Nullable MultiValueMap<String, String> queryParams);
 	
+	public PageResponse<UserCourseActivity> getUserCourseActivity();
+	
 	public PageResponse<UserCourseActivity> getUserCourseActivity(@Nullable MultiValueMap<String, String> queryParams);
+	
+	public PageResponse<UserProgress> getUserProgress();
 	
 	public PageResponse<UserProgress> getUserProgress(@Nullable MultiValueMap<String, String> queryParams);	
 }
--- app/udemy-connector/src/main/java/com/frager/oreport/udemyconnector/config/WebConfig.java
+++ app/udemy-connector/src/main/java/com/frager/oreport/udemyconnector/config/WebConfig.java
@@ -1,14 +1,28 @@
 package com.frager.oreport.udemyconnector.config;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebConfig {
-	
-	@Bean
-	public RestTemplate defaultRestTemplate() {
-		return new RestTemplate();
+
+	@Bean("udemy-client-webclient-builder")
+	public WebClient.Builder getWebClientBuilder(@Value("${udemy.client.id:}") String user,
+			@Value("${udemy.client.secret:}") String pass, @Value("${udemy.client.auth-header:}") String authHeader) {
+		
+		if (authHeader == null || authHeader.isEmpty()) {
+			byte[] encodedAuth = Base64.getEncoder().encode((user + ":" + pass).getBytes(Charset.forName("US-ASCII")));
+			authHeader = "Basic " + new String(encodedAuth);
+		}
+		
+		HttpHeaders httpHeadersForUdemy = new HttpHeaders();
+		httpHeadersForUdemy.add("Authorization", authHeader);
+		return WebClient.builder().defaultHeaders(defaultHeaders -> defaultHeaders.addAll(httpHeadersForUdemy));
 	}
 }
