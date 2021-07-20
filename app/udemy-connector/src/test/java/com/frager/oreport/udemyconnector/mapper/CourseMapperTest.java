049682cc85dbbd3de3986bd0fe8817fe814e3da7
Fradantim <fradantim@gmail.com>
2021-07-19 22:44:04 -0300

    Primeros tests T_T

---
+++ app/udemy-connector/src/test/java/com/frager/oreport/udemyconnector/mapper/CourseMapperTest.java
@@ -0,0 +1,32 @@
+package com.frager.oreport.udemyconnector.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.frager.oreport.udemyconnector.model.Course;
+import com.udemy.model.ListedCourse;
+import com.udemy.model.SingleCourse;
+
+class CourseMapperTest {
+
+	@Test
+	void testMapperFromSingle() {
+		SingleCourse sc = new SingleCourse();
+		sc.setId(1);
+		
+		Course c = CourseMapper.fromSingleCourse(sc);
+		assertEquals(sc.getId(), c.getId());
+	}
+	
+	@Test
+	void testMapperFromListed() {
+		ListedCourse lc = new ListedCourse();
+		lc.setId(1);
+		
+		Course c = CourseMapper.fromListedCourse(lc);
+		assertEquals(lc.getId(), c.getId());
+	}
+}
