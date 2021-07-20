3aa7021ca73064119f2b8004da67af634cb3848f
Fradantim <fradantim@gmail.com>
2021-07-19 23:10:53 -0300

    Generalizacion de test

--- app/udemy-connector/src/test/java/com/frager/oreport/udemyconnector/mapper/CourseMapperTest.java
+++ app/udemy-connector/src/test/java/com/frager/oreport/udemyconnector/mapper/CourseMapperTest.java
@@ -10,21 +10,47 @@ import com.udemy.model.SingleCourse;
 
 class CourseMapperTest {
 
-	@Test
-	void testMapperFromSingle() {
-		SingleCourse sc = new SingleCourse();
-		sc.setId(1);
-		
-		Course c = CourseMapper.fromSingleCourse(sc);
-		assertEquals(sc.getId(), c.getId());
+	private void fillSampleCourse(com.udemy.model.Course c) {
+		c.setDescription("Una descripcion");
+		c.setEstimatedContentLength(177013);
+		c.setId(1);
+		c.setNumberOfLectures(123);
+		c.setNumberOfPracticeTests(456);
+		c.setNumberOfQuizzes(789);
+		c.setTitle("Un titulo");
+		c.setUrl("Una url");
 	}
-	
+
+	private void assertCourseKeepsContents(com.udemy.model.Course udemyCourse, Course oreportCourse) {
+		assertEquals(udemyCourse.getDescription(), oreportCourse.getDescription());
+		assertEquals(udemyCourse.getEstimatedContentLength(), oreportCourse.getEstimatedContentLength());
+		assertEquals(udemyCourse.getId(), oreportCourse.getId());
+		assertEquals(udemyCourse.getNumberOfLectures(), oreportCourse.getNumberOfLectures());
+		assertEquals(udemyCourse.getNumberOfPracticeTests(), oreportCourse.getNumberOfPracticeTests());
+		assertEquals(udemyCourse.getNumberOfQuizzes(), oreportCourse.getNumberOfQuizzes());
+		assertEquals(udemyCourse.getTitle(), oreportCourse.getTitle());
+		assertEquals(udemyCourse.getUrl(), oreportCourse.getUrl());
+	}
+
 	@Test
-	void testMapperFromListed() {
+	void testMapperFromSingleCourse() {
+		SingleCourse sc = new SingleCourse();
+		fillSampleCourse(sc);
+
+		// reservado para mas atributos de SingleCourse
+
+		Course c = CourseMapper.fromSingleCourse(sc);
+		assertCourseKeepsContents(sc, c);
+	}
+
+	@Test
+	void testMapperFromListedCourse() {
 		ListedCourse lc = new ListedCourse();
-		lc.setId(1);
-		
+		fillSampleCourse(lc);
+
+		// reservado para mas atributos de ListedCourse
+
 		Course c = CourseMapper.fromListedCourse(lc);
-		assertEquals(lc.getId(), c.getId());
+		assertCourseKeepsContents(lc, c);
 	}
 }
