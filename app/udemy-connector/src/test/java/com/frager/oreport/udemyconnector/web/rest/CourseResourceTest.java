00c9cfebc003fb073f2ae13f8e1df7b2d3bf81e7
Fradantim <fradantim@gmail.com>
2021-07-24 17:35:07 -0300

    **some** assertion...

---
+++ app/udemy-connector/src/test/java/com/frager/oreport/udemyconnector/web/rest/CourseResourceTest.java
@@ -0,0 +1,10 @@
+package com.frager.oreport.udemyconnector.web.rest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource("classpath:secrets.mock.properties")
+class CourseResourceTest {
+
+}
