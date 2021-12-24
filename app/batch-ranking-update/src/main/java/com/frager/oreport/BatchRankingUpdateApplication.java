699a83a33e34c7450a1b56b502fb546b14b17aa5
Otero Bello <a.otero.bello@accenture.com>
2021-12-24 07:20:45 -0300

    Se agrega el scheduler batch-ranking-update

---
+++ app/batch-ranking-update/src/main/java/com/frager/oreport/BatchRankingUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.frager.oreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchRankingUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchRankingUpdateApplication.class, args);
+	}
+
+}
